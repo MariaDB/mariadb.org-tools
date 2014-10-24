53734c6a8060e00d2dcf1a04d2dc40c3cde6ce48
Daniel Bartholomew <dbart@askmonty.org>
2014-10-24 17:51:49 +0300

    Various updates to mkrepo-ubuntu.sh script to make it more readable and incorporate the latest changes.

--- buildbot/mkrepo-ubuntu.sh
+++ buildbot/mkrepo-ubuntu.sh
@@ -1,32 +1,75 @@
 #! /bin/sh
+#===============================================================================
+#
+#          FILE:  mkrepo-ubuntu.sh
+# 
+#         USAGE:  $0 <galera_pkgs?> <enterprise?> <reponame> <archive_dir>
+# 
+#   DESCRIPTION:  A script to generate the Ubuntu repositories for MariaDB
+#                 Ubuntu packages.
+#
+#                 The script copies files from the archive directory into
+#                 separate directories for each distribution/cpu combination
+#                 (just like they are stored in the archive directory). For
+#                 best results, it should be run within an empty directory.
+#
+#                 After running the script, the directories are uploaded to the
+#                 mirrors, replacing the previous version in that series (i.e.
+#                 the 10.0.15 files are replaced by the 10.0.16 files and the
+#                 10.1.1 files are replaced by the 10.1.2 files, and so on).
+# 
+#===============================================================================
 
-set -eux
+# Right off the bat we want to log everything we're doing and exit immediately
+# if there's an error
+set -ex
+  # -e  Exit immediately if a simple command exits with a non-zero status,
+  #     unless the command that fails is part of an until or  while loop, part
+  #     of an if statement, part of a && or || list, or if the command's return
+  #     status is being inverted using !.  -o errexit
+  #
+  # -x  Print a trace of simple commands and their arguments after they are
+  #     expanded and before they are executed.
 
+#-------------------------------------------------------------------------------
+#  Set command-line options
+#-------------------------------------------------------------------------------
 GALERA="$1"                       # copy in galera packages? 'yes' or 'no'
 ENTERPRISE="$2"                   # is this an enterprise release? 'yes' or 'no'
 REPONAME="$3"                     # name of the dir, usually 'ubuntu'
 ARCHDIR="$4"                      # path to the packages
 
+#-------------------------------------------------------------------------------
+#  Variables which are not set dynamically (because they don't change often)
+#-------------------------------------------------------------------------------
+galera_versions="25.3.5"                    # Version of galera in repos
+jemalloc_dir="/ds413/vms-customizations"    # Location of custom jemalloc pkgs
+galera_dir="/ds413/galera"                  # Location of custom galera pkgs
+ubuntu_dists="lucid precise trusty"
+
+#-------------------------------------------------------------------------------
+#  Main Script
+#-------------------------------------------------------------------------------
+# Get the GPG daemon running so we don't have to keep entering the password for
+# the GPG key every time we sign a package
 eval $(gpg-agent --daemon)
 
-#ubuntu_dists="lucid precise quantal saucy trusty"
-ubuntu_dists="lucid precise saucy trusty"
-#ubuntu_dists="precise saucy trusty"
-#ubuntu_dists="precise trusty"
-galera_versions="25.3.5"
-#galera_versions="25.3.5 25.2.9"
-#galera_versions="25.3.2 25.2.8"
-#galera_versions="25.3.2"
-
-#jemalloc_dir="/home/dbart/jemalloc"
-jemalloc_dir="/ds413/vms-customizations/"
-
+# At this point, all variables should be set. Print a usage message if the
+# ${ARCHDIR} variable is not set (the last of the command-line variables).
 if [ ! -d "$ARCHDIR" ] ; then
-    echo 1>&2 "Usage: $0 <galera_pkgs?> <enterprise?> <reponame> <archive directory>"
+    echo 1>&2 "Usage: $0 <galera_pkgs?> <enterprise?> <reponame> <archive_dir>"
     echo 1>&2 "example: $0 yes no ubuntu /media/backup/archive/pack/10.0/build-1234"
     exit 1
 fi
 
+# After this point, we tread unset variables as an error
+set -u
+  # -u  Treat unset variables as an error when performing parameter expansion.
+  #     An error message will be written to the standard error, and a
+  #     non-interactive shell will exit.
+
+# If this is an "Enterprise" MariaDB release, sign with the mariadb.com key,
+# otherwise, sign with the mariadb.org key
 if [ "${ENTERPRISE}" = "yes" ]; then
   origin="MariaDB Enterprise"
   description="MariaDB Enterprise Repository"
@@ -36,7 +79,6 @@ else
   description="MariaDB Repository"
   sign_with="package-signing-key@mariadb.org"
 fi
-
 
 mkdir "$REPONAME"
 cd "$REPONAME"
@@ -80,55 +122,33 @@ for dist in ${ubuntu_dists}; do
   reprepro --basedir=. include ${dist} $ARCHDIR/kvm-deb-${dist}-amd64/debs/binary/mariadb-*_amd64.changes
   for file in $(find "$ARCHDIR/kvm-deb-${dist}-x86/" -name '*_i386.deb'); do reprepro --basedir=. includedeb ${dist} ${file} ; done
 
+  # Add in custom jemalloc packages for distros that need them
   case  ${dist} in
     "lucid")
-      #mkdir -vp pool/main/j/jemalloc
-      #rsync -av ${jemalloc_dir}/jemalloc-${dist}-amd64/jemalloc*orig.tar.bz2 pool/main/j/jemalloc/
-      #reprepro --basedir=. include ${dist} ${jemalloc_dir}/jemalloc-${dist}-amd64/jemalloc*_amd64.changes
-
-
-
-
       for file in $(find "${jemalloc_dir}/${dist}-amd64/" -name '*_amd64.deb'); do reprepro --basedir=. includedeb ${dist} ${file} ; done
       for file in $(find "${jemalloc_dir}/${dist}-i386/" -name '*_i386.deb'); do reprepro --basedir=. includedeb ${dist} ${file} ; done
-      #for file in $(find "${jemalloc_dir}/jemalloc-debs/" -name '*.deb'); do reprepro --basedir=. includedeb ${dist} ${file} ; done
       ;;
     "precise")
-      #mkdir -vp pool/main/j/jemalloc
-      #rsync -av ${jemalloc_dir}/jemalloc-${dist}-amd64/jemalloc*orig.tar.bz2 pool/main/j/jemalloc/
-      #reprepro --basedir=. include ${dist} ${jemalloc_dir}/jemalloc-${dist}-amd64/jemalloc*_amd64.changes
-
-
-
-
       for file in $(find "${jemalloc_dir}/${dist}-amd64/" -name '*_amd64.deb'); do reprepro --basedir=. includedeb ${dist} ${file} ; done
       for file in $(find "${jemalloc_dir}/${dist}-i386/" -name '*_i386.deb'); do reprepro --basedir=. includedeb ${dist} ${file} ; done
-      #for file in $(find "${jemalloc_dir}/jemalloc-debs/" -name '*.deb'); do reprepro --basedir=. includedeb ${dist} ${file} ; done
       ;;
     "quantal")
-      #mkdir -vp pool/main/j/jemalloc
-      #rsync -av ${jemalloc_dir}/jemalloc-${dist}-amd64/jemalloc*orig.tar.bz2 pool/main/j/jemalloc/
-      #reprepro --basedir=. include ${dist} ${jemalloc_dir}/jemalloc-${dist}-amd64/jemalloc*_amd64.changes
-
-
-
-
       for file in $(find "${jemalloc_dir}/${dist}-amd64/" -name '*_amd64.deb'); do reprepro --basedir=. includedeb ${dist} ${file} ; done
       for file in $(find "${jemalloc_dir}/${dist}-i386/" -name '*_i386.deb'); do reprepro --basedir=. includedeb ${dist} ${file} ; done
-      #for file in $(find "${jemalloc_dir}/jemalloc-debs/" -name '*.deb'); do reprepro --basedir=. includedeb ${dist} ${file} ; done
       ;;
     * )
       echo "no custom jemalloc packages for ${dist}"
       ;;
   esac
 
-  #for file in $(find "/home/dbart/galera/" -name '*.deb' -not -name '*wheezy*'); do reprepro -S optional -P misc --basedir=. includedeb ${dist} ${file} ; done
+  # Copy in galera packages if requested
   if [ ${GALERA} = "yes" ]; then
     for gv in ${galera_versions}; do
-      for file in $(find "/home/dbart/galera-${gv}/" -name "*${dist}*.deb"); do reprepro -S optional -P misc --basedir=. includedeb ${dist} ${file} ; done
+      for file in $(find "${galera_dir}/galera-${gv}/" -name "*${dist}*.deb"); do reprepro -S optional -P misc --basedir=. includedeb ${dist} ${file} ; done
     done
   fi
 done
 
+# Create md5sums of .deb packages
 md5sum ./pool/main/*/*/*.deb >> md5sums.txt
 
