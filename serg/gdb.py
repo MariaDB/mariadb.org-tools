9abb154597b5ba6518140e41b03a30fc80cebcf6
Sergei Golubchik <sergii@pisem.net>
2017-03-23 17:41:26 +0100

    pretty printer for sql_mode_t
    and support printing typedefs not as base types (as sql_mode_t is ulonglong)

--- serg/gdb.py
+++ serg/gdb.py
@@ -27,8 +27,10 @@ def PrettyPrinter(func):
                 prefix = '({}) {:#08x} '.format(str(val.type), long(val))
                 try: val = val.dereference()
                 except: return None
-            typename = val.type.unqualified().strip_typedefs().name
-            if typename == self.name:
+            valtype=val.type.unqualified()
+            if valtype.name == self.name:
+                return self.PrettyPrinterWrapper(prefix, val, self.cb)
+            if valtype.strip_typedefs().name == self.name:
                 return self.PrettyPrinterWrapper(prefix, val, self.cb)
             return None
 
@@ -48,3 +50,20 @@ def st_bitmap(val):
     for i in range((val['n_bits']+7)//8):
         s = format(int(val['bitmap'][i]), '032b') + s
     return "b'" + s[-int(val['n_bits']):] + "'"
+
+@PrettyPrinter
+def sql_mode_t(val):
+    s=''
+    modes=['REAL_AS_FLOAT', 'PIPES_AS_CONCAT', 'ANSI_QUOTES', 'IGNORE_SPACE',
+           'IGNORE_BAD_TABLE_OPTIONS', 'ONLY_FULL_GROUP_BY',
+           'NO_UNSIGNED_SUBTRACTION', 'NO_DIR_IN_CREATE', 'POSTGRESQL',
+           'ORACLE', 'MSSQL', 'DB2', 'MAXDB', 'NO_KEY_OPTIONS',
+           'NO_TABLE_OPTIONS', 'NO_FIELD_OPTIONS', 'MYSQL323', 'MYSQL40',
+           'ANSI', 'NO_AUTO_VALUE_ON_ZERO', 'NO_BACKSLASH_ESCAPES',
+           'STRICT_TRANS_TABLES', 'STRICT_ALL_TABLES', 'NO_ZERO_IN_DATE',
+           'NO_ZERO_DATE', 'INVALID_DATES', 'ERROR_FOR_DIVISION_BY_ZERO',
+           'TRADITIONAL', 'NO_AUTO_CREATE_USER', 'HIGH_NOT_PRECEDENCE',
+           'NO_ENGINE_SUBSTITUTION', 'PAD_CHAR_TO_FULL_LENGTH']
+    for i in range(0,len(modes)):
+        if val & (1 << i): s += ',' + modes[i]
+    return s[1:]
