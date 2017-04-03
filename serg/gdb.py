62210f5904dde5e93ad6e21ef8bb1c886c3db597
Sergei Golubchik <sergii@pisem.net>
2017-04-03 11:13:25 +0200

    gdb.py reformat

--- serg/gdb.py
+++ serg/gdb.py
@@ -6,37 +6,38 @@ except: long=int
 
 def PrettyPrinter(arg):
 
-    class PrettyPrinterWrapperWrapper:
-
-        class PrettyPrinterWrapper:
-            def __init__(self, prefix, val, cb):
-                self.prefix = prefix
-                self.val = val
-                self.cb = cb
-            def to_string(self):
-                return self.prefix + self.cb(self.val)
-
-        def __init__(self, name, cb):
-            self.name = name
-            self.enabled = True
-            self.cb = cb
-
-        def __call__(self, val):
-            prefix = ''
-            if val.type.code == gdb.TYPE_CODE_PTR:
-                prefix = '({}) {:#08x} '.format(str(val.type), long(val))
-                try: val = val.dereference()
-                except: return None
-            valtype=val.type.unqualified()
-            if valtype.name == self.name:
-                return self.PrettyPrinterWrapper(prefix, val, self.cb)
-            if valtype.strip_typedefs().name == self.name:
-                return self.PrettyPrinterWrapper(prefix, val, self.cb)
-            return None
-
     name = getattr(arg, '__name__', arg)
 
     def PrettyPrinterWrapperWrapperWrapper(func):
+
+        class PrettyPrinterWrapperWrapper:
+
+            class PrettyPrinterWrapper:
+                def __init__(self, prefix, val, cb):
+                    self.prefix = prefix
+                    self.val = val
+                    self.cb = cb
+                def to_string(self):
+                    return self.prefix + self.cb(self.val)
+
+            def __init__(self, name, cb):
+                self.name = name
+                self.enabled = True
+                self.cb = cb
+
+            def __call__(self, val):
+                prefix = ''
+                if val.type.code == gdb.TYPE_CODE_PTR:
+                    prefix = '({}) {:#08x} '.format(str(val.type), long(val))
+                    try: val = val.dereference()
+                    except: return None
+                valtype=val.type.unqualified()
+                if valtype.name == self.name:
+                    return self.PrettyPrinterWrapper(prefix, val, self.cb)
+                if valtype.strip_typedefs().name == self.name:
+                    return self.PrettyPrinterWrapper(prefix, val, self.cb)
+                return None
+
         pp=PrettyPrinterWrapperWrapper(name, func)
         gdb.printing.register_pretty_printer(None, pp, True)
         return func
