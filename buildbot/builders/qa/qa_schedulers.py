1bbb1c39007a551a0f429a28bf3e93f61250492c
Daniel Bartholomew <dbart@mariadb.com>
2017-06-14 03:03:21 +0300

    automatic builders/qa/qa_schedulers.py commit

--- buildbot/builders/qa/qa_schedulers.py
+++ buildbot/builders/qa/qa_schedulers.py
@@ -1,4 +1,4 @@
-qaTargetTrees = ["5.5", "10.0", "10.1", "10.2", "10.3", "bb-5.5-elenst*", "bb-10.0-elenst*", "bb-10.1-elenst*", "bb-10.2-elenst*", "bb-10.3-elenst*"]
+qaTargetTrees = ["5.5", "10.0", "10.1", "10.2", "10.3", "bb-5.5-elenst*", "bb-10.0-elenst*", "bb-10.1-elenst*", "bb-10.2-elenst*", "bb-10.3-elenst*", "bb-10.2-marko"]
 
 def isTargetQA(step):
   return step.getProperty("branch") in qaTargetTrees
