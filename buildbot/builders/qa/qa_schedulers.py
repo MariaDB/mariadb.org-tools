d5d1b69456322fbe5c1f7676a4acd62db749fb79
Daniel Bartholomew <dbart@mariadb.com>
2017-08-29 03:03:27 +0300

    automatic builders/qa/qa_schedulers.py commit

--- buildbot/builders/qa/qa_schedulers.py
+++ buildbot/builders/qa/qa_schedulers.py
@@ -1,12 +1,17 @@
-qaTargetTrees_10x = [ "10.0", "10.1", "10.2", "10.3", "bb-10.0-elenst*", "bb-10.1-elenst*", "bb-10.2-elenst*", "bb-10.3-elenst*", "bb-10.0-marko", "bb-10.1-marko", "bb-10.2-marko", "bb-10.3-marko", "bb-10.2-ext"]
+qaTargetTrees_10x = ["10.0", "10.1", "10.2", "10.3", "bb-10.0-elenst*", "bb-10.1-elenst*", "bb-10.2-elenst*", "bb-10.3-elenst*", "bb-10.2-ext"]
 
 qaTargetTrees = ["5.5", "bb-5.5-elenst*"] + qaTargetTrees_10x
+
+qaTargetInnoDB = ["10.0", "10.1", "10.2", "10.3", "bb-10.0-marko", "bb-10.1-marko", "bb-10.2-marko", "bb-10.3-marko"]
 
 def isTargetQA(step):
   return step.getProperty("branch") in qaTargetTrees
 
 def isTargetQA_10x(step):
   return step.getProperty("branch") in qaTargetTrees_10x
+
+def isTargetQA_InnoDB(step):
+  return step.getProperty("branch") in qaTargetInnoDB
 
 ###########################
 # Server tests
@@ -22,6 +27,12 @@ c['schedulers'].append(Triggerable(
         name="kvm-sched-qa-trees-10x",
         builderNames=[
                       "qa-kvm-linux",
+                     ]))
+
+c['schedulers'].append(Triggerable(
+        name="kvm-sched-qa-innodb",
+        builderNames=[
+                      "qa-innodb-upgrade",
                      ]))
 
 
