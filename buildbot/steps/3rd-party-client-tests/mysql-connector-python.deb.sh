6fd5105cd35d4fe30e5bbad9c0a94f2b03c0faed
Elena Stepanova <elenst@mariadb.com>
2020-11-12 15:37:33 +0200

    Fix redirection

--- buildbot/steps/3rd-party-client-tests/mysql-connector-python.deb.sh
+++ buildbot/steps/3rd-party-client-tests/mysql-connector-python.deb.sh
@@ -5,6 +5,6 @@ cd mysql-connector-python-*/
 sed -ie 's/-for python/for python/' debian/rules
 sed -ie '/(5, 7, /,/^$/d' tests/mysqld.py 
 
-dh build |tee build.log 2>&1 
+dh build 2>&1 | tee build.log
 
 grep '^\(FAIL\|ERROR\):' build.log
