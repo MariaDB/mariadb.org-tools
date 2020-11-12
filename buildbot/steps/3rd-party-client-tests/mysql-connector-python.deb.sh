9874972a3bba518b8156bb60eea434b52356954d
Elena Stepanova <elenst@mariadb.com>
2020-11-12 15:36:52 +0200

    Also install python, as some builders only have python2

--- buildbot/steps/3rd-party-client-tests/mysql-connector-python.deb.sh
+++ buildbot/steps/3rd-party-client-tests/mysql-connector-python.deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-sudo apt-get install -y python
+sudo apt-get install -y python python3
 cd mysql-connector-python-*/
 sed -ie 's/-for python/for python/' debian/rules
 sed -ie '/(5, 7, /,/^$/d' tests/mysqld.py 
