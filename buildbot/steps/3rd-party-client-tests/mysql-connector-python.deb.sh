cef633e2e2da3756490a0418db74dc5e83a50ac0
Elena Stepanova <elenst@mariadb.com>
2020-11-15 11:51:03 +0200

    8.0 connectors require a different approach

--- buildbot/steps/3rd-party-client-tests/mysql-connector-python.deb.sh
+++ buildbot/steps/3rd-party-client-tests/mysql-connector-python.deb.sh
@@ -1,11 +1,27 @@
 #!/bin/bash -x
 
-sudo apt-get install -y python python3 python3-setuptools python-setuptools
+# python3-setuptools python-setuptools
+sudo apt-get install -y python python3 dh-python
 cd mysql-connector-python-*/
-sed -ie 's/-for python/for python/' debian/rules
-sed -ie '/(5, 7, /,/^$/d' tests/mysqld.py
-sed -ie "s/\(matches = re\.match(r'\.\*Ver (\\\d\))/\1\+)/g" tests/mysqld.py
 
-dh build 2>&1 | tee build.log
+sed -ie "s/override_dh_auto_test/override_dh_auto_test_orig/g" debian/rules
+
+cat >> debian/rules << EOF
+
+override_dh_auto_test:
+ifeq (,\$(findstring nocheck,\$(DEB_BUILD_OPTIONS)))
+	-for python in \$(PYTHON2) \$(PYTHON3); do \\
+               LIB=\$\$(\$\$python -c "from distutils.command.build import build ; from distutils.core import Distribution ; b = build(Distribution()) ; b.finalize_options() ; print (b.build_purelib)") ;\\
+               mkdir -p /tmp/con-python/ ; \\
+               PYTHONPATH=\$(CURDIR)/\$\$LIB \$\$python unittests.py --with-mysql=/usr/ --mysql-topdir=/tmp/con-python/ --verbosity=2 --bind-address=:: --host=::1 --stats ; \\
+               rm -rf /tmp/con-python/ ; \\
+       done
+endif
+EOF
+
+sed -ie 's/^\(\s*\)def _get_version(self):.*$/\1def _get_version(self):\n\1\1return (5,6,99)/' tests/mysqld.py
+sed -ie "s/'--is-wheel'//" tests/__init__.py
+
+make -f debian/rules build 2>&1 | tee build.log
 
 grep '^\(FAIL\|ERROR\):' build.log | tee /tmp/test.out
