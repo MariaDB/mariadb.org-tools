3510fb87c0676498aa1cc48b747982a90735b51a
Daniel Black <daniel@mariadb.org>
2020-12-11 08:45:00 +1100

    bb.org: eco installdb.sh - root with grant option

--- buildbot.mariadb.org/dockerfiles/ecofiles/installdb.sh
+++ buildbot.mariadb.org/dockerfiles/ecofiles/installdb.sh
@@ -27,4 +27,4 @@ done
 
 [ -S /tmp/mysql.sock ] || exit 1
 
-/usr/local/mariadb/bin/mysql -e 'create user if not exists root@localhost ; grant all on *.* TO root@localhost'
+/usr/local/mariadb/bin/mysql -e 'create user if not exists root@localhost ; grant all on *.* TO root@localhost with grant option'
