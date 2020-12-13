7adc8b328305b6a55d7a412c78665b84c82032a1
Daniel Black <daniel@mariadb.org>
2020-12-14 08:32:35 +1100

    bb.org: dbdeployer - tmp hardcode version

--- buildbot.mariadb.org/dockerfiles/ecofiles/test-dbdeployer.sh
+++ buildbot.mariadb.org/dockerfiles/ecofiles/test-dbdeployer.sh
@@ -13,11 +13,11 @@ fi
 # curl -L -s -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/datacharmer/dbdeployer/releases/latest | grep -B 4 linux.tar.gz\"
 case "${1}" in
 	dbdeployerfetch)
-		f=dbdeployer-"${2}".linux
+		f=dbdeployer-1.57.0.linux
 		[ -f "/tmp/$f" ] || \
 			curl -L -s -H 'Accept: application/octet-stream' https://api.github.com/repos/datacharmer/dbdeployer/releases/assets/"${2}" \
 			| tar -zxf - -C /tmp
-		ln -s /tmp/dbdeployer-${2}.linux dbeployer
+		ln -s /tmp/"$f" dbeployer
 		;;
 	init)
 		mkdir /tmp/opt
