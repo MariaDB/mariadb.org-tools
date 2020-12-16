9436dd7fad61bd815051e72465499ff4bf66a544
Daniel Black <daniel@mariadb.org>
2020-12-16 16:54:20 +1100

    bb.org: eco installdb.sh compatibility for 10.2, 10.3

--- buildbot.mariadb.org/dockerfiles/ecofiles/installdb.sh
+++ buildbot.mariadb.org/dockerfiles/ecofiles/installdb.sh
@@ -27,4 +27,5 @@ done
 
 [ -S /tmp/mysql.sock ] || exit 1
 
-/usr/local/mariadb/bin/mysql -e 'create user if not exists root@localhost; set password for root@localhost = password("") ; grant all on *.* TO root@localhost with grant option; show create user root@localhost; show grants for root@localhost'
+/usr/local/mariadb/bin/mysql -e 'create user if not exists root@localhost; set password for root@localhost = password("") ; grant all on *.* TO root@localhost with grant option; show create user root@localhost; show grants for root@localhost' \
+	|| /usr/local/mariadb/bin/mysql -e 'show create user root@locahost; show grants for root@localhost'
