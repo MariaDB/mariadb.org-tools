15245d5cd9e658706b2443162118e9788c26e97a
Daniel Black <daniel@mariadb.org>
2021-01-11 11:44:20 +1100

    bb.org: mysqljs correct test-statistics. Enable intergration test

--- buildbot.mariadb.org/dockerfiles/ecofiles/test-mysqljs.sh
+++ buildbot.mariadb.org/dockerfiles/ecofiles/test-mysqljs.sh
@@ -18,6 +18,9 @@ then
   git checkout $1
 fi
 
+# awaiting fix https://github.com/mysqljs/mysql/pull/2442
+sed -i -e '/flush_tables/d' test/integration/connection/test-statistics.js
+
 npm install
 # Run the unit tests (probably should be controlled with worker variable)
 # If unit==1 run unit test else run integration test
@@ -25,6 +28,6 @@ cd ./test
 FILTER=unit npm test
 
 # Run integration test - we are more interested in this!
-#/usr/local/mariadb/bin/mysql -u root -e "CREATE DATABASE IF NOT EXISTS node_mysql_test"
-#MYSQL_HOST=localhost MYSQL_PORT=3306 MYSQL_DATABASE=node_mysql_test MYSQL_USER=root MYSQL_PASSWORD= FILTER=integration npm test
+/usr/local/mariadb/bin/mysql -u root -e "CREATE DATABASE IF NOT EXISTS node_mysql_test"
+MYSQL_HOST=localhost MYSQL_PORT=3306 MYSQL_DATABASE=node_mysql_test MYSQL_USER=root MYSQL_PASSWORD= FILTER=integration npm test
 
