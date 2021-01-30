01f5e93a4ec2c8ffb1b4947071ac274f164e813b
Vlad Bogolin <vladbogolin@yahoo.com>
2021-01-30 19:02:47 +0100

    bb.org: Add package rotation script

--- buildbot.mariadb.org/scripts/bb_package_rotation.sh
+++ buildbot.mariadb.org/scripts/bb_package_rotation.sh
@@ -29,12 +29,14 @@ touch $VAR_SCRIPT_LOCK
 [[ -d $VAR_NFS_DIR ]] || err "$VAR_NFS_DIR not found"
 [[ -d $VAR_LOCAL_DIR ]] || err "$VAR_LOCAL_DIR not found"
 
+IGNORE_DIR="helper_files"
+
 typeset -r VAR_NFS_USAGE=$(df $VAR_NFS_DIR | tail -1 | awk '{print $5}')
 if ((${VAR_NFS_USAGE/\%/} >= 70)); then
   echo_date "cleaning 10 oldest build"
   cd $VAR_NFS_DIR || err "cd $VAR_NFS_DIR"
   # shellcheck disable=SC2012
-  ls -rt | head -10 | xargs rm -rv || err "cleaning 10 oldest build"
+  ls -rt -I $IGNORE_DIR | head -10 | xargs rm -rv || err "cleaning 10 oldest build"
 fi
 
 typeset -r VAR_LOCAL_USAGE=$(df $VAR_LOCAL_DIR | tail -1 | awk '{print $5}')
@@ -43,7 +45,7 @@ if ((${VAR_LOCAL_USAGE/\%/} >= 77)); then
   cd $VAR_LOCAL_DIR || err "cd $VAR_LOCAL_DIR"
   [[ -L older_builds ]] || err "older_builds not found"
   # shellcheck disable=SC2012
-  for dir in $(ls -rt | grep -v older_builds | head -10); do
+  for dir in $(ls -rt -I $IGNORE_DIR | grep -v older_builds | head -10); do
     mv -v "$dir" older_builds/ || err "moving $dir to older_builds/"
   done
 fi
