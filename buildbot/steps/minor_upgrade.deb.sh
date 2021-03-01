4ddb77e0fc98e2402dd4873aa90d76da1d62dd5c
Daniel Black <daniel@mariadb.org>
2021-03-02 10:00:56 +1100

    minor_upgrade.deb - script fix
    fixes:
    
    ++ -e /home/buildbot/mariadb.org-tools-master/buildbot/baselines/ldd.10.2.bionic.amd64
    ./steps/minor_upgrade.deb.sh: line 519: -e: command not found

--- buildbot/steps/minor_upgrade.deb.sh
+++ buildbot/steps/minor_upgrade.deb.sh
@@ -516,7 +516,7 @@ case "$branch" in
   set -o pipefail
   if [ "$test_mode" == "all" ] ; then
     set -o pipefail
-    if -e $script_home/baselines/ldd.${major_version}.${version_name}.${arch} ; then
+    if [ -e $script_home/baselines/ldd.${major_version}.${version_name}.${arch} ]; then
       ldd_baseline=$script_home/baselines/ldd.${major_version}.${version_name}.${arch}
     else
       ldd_baseline=/home/buildbot/ldd.old
