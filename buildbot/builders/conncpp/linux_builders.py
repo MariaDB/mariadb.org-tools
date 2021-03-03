5471d5a1aa752e299326c18049c6a53d54f6876b
Daniel Bartholomew <dbart@mariadb.com>
2021-03-03 03:03:10 +0200

    automatic builders/conncpp/linux_builders.py commit

--- buildbot/builders/conncpp/linux_builders.py
+++ buildbot/builders/conncpp/linux_builders.py
@@ -162,12 +162,9 @@ conncpp_linux_step4_testsrun
             'factory': linux_connector_cpp,
             "slavenames": connector_slaves,
             "category": "connectors"}
-######################## bld_linux_connector_oddbc - END #####################
+######################## bld_linux_connector_cpp - END #####################
 
-######################## Current GA/stable version builders ######################
-#################$### Current GA/stable version builders - END ###################
-
-######################## New (unstable) version builders ######################
+######################## "Normal" builders ######################
 bld_centos8_amd64_connector_cpp= bld_linux_connector_cpp("ccpp-centos8-amd64", "vm-centos8-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON ");
 bld_stretch_amd64_connector_cpp= bld_linux_connector_cpp("ccpp-stretch-amd64", "vm-stretch-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON ");
 
@@ -181,9 +178,8 @@ bld_groovy_amd64_connector_cpp= bld_linux_connector_cpp("ccpp-groovy-amd64", "vm
 bld_buster_amd64_connector_cpp= bld_linux_connector_cpp("ccpp-buster-amd64", "vm-buster-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON");
 bld_fedora32_amd64_connector_cpp= bld_linux_connector_cpp("ccpp-fedora32-amd64", "vm-fedora32-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON");
 bld_fedora33_amd64_connector_cpp= bld_linux_connector_cpp("ccpp-fedora33-amd64", "vm-fedora33-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON");
-bld_sles12_amd64_connector_cpp= bld_linux_connector_cpp("ccpp-sles12-amd64", "vm-sles123-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON");
 
-##################### Building with old gcc with the hack ###################
+##################### Building with the hack for platforms with old gcc ##################
 
 def bld_linux_connector_cpp_with_hack(name, kvm_image, cflags, cmake_params):
     linux_connector_cpp= BuildFactory()
@@ -227,4 +223,55 @@ conncpp_linux_step4_testsrun
             'factory': linux_connector_cpp,
             "slavenames": connector_slaves,
             "category": "connectors"}
+
 bld_centos7_amd64_connector_cpp= bld_linux_connector_cpp_with_hack("ccpp-centos7-amd64", "vm-centos74-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON ");
+
+##################### Building with gcc-5 for platforms with >1 gcc installed, and default is too old  ##################
+
+def bld_linux_connector_cpp_with_gcc5(name, kvm_image, cflags, cmake_params):
+    linux_connector_cpp= BuildFactory()
+    args= ["--port="+getport(), "--user=buildbot", "--smp=4", "--cpu=qemu64"]
+    linux_connector_cpp.addStep(ShellCommand(
+        description=["cleaning", "build", "dir"],
+        descriptionDone=["clean", "build", "dir"],
+        command=["sh", "-c", "rm -Rf ../build/*"]))
+    linux_connector_cpp.addStep(ShellCommand(
+        description=["rsyncing", "VMs"],
+        descriptionDone=["rsync", "VMs"],
+        doStepIf=(lambda(step): step.getProperty("slavename") != "bb01"),
+        haltOnFailure=True,
+        command=["rsync", "-a", "-v", "-L",
+                 "bb01.mariadb.net::kvm/vms/"+kvm_image+"-connector.qcow2",
+                 "/kvm/vms/"]))
+    linux_connector_cpp.addStep(Compile(
+        description=["building", "linux-connctor_cpp"],
+        descriptionDone=["build", "linux-connector_cpp"],
+        timeout=3600,
+        env={"TERM": "vt102"},
+        command=["runvm", "--base-image=/kvm/vms/"+kvm_image+"-connector.qcow2"] + args +["vm-tmp-"+getport()+".qcow2",
+        "rm -Rf buildbot && mkdir buildbot",
+        WithProperties("""
+export CFLAGS="${CFLAGS}"""+ cflags + """" """ +
+conncpp_linux_step0_checkout + """
+export CC=/usr/bin/gcc-5
+export CXX=/usr/bin/g++-5
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCONC_WITH_UNIT_TESTS=Off -DPACKAGE_PLATFORM_SUFFIX=$HOSTNAME""" + cmake_params + """ ../src""" +
+conncpp_linux_step1_build +
+conncpp_linux_step2_serverinstall +
+conncpp_linux_step3_packagetest +
+conncpp_linux_step4_testsrun
+),
+        "= scp -r -P "+getport()+" "+kvm_scpopt+" buildbot@localhost:/home/buildbot/build/mariadb*tar.gz .",
+        ]))
+    linux_connector_cpp.addStep(SetPropertyFromCommand(
+        property="bindistname",
+        command=["sh", "-c", WithProperties("basename `ls mariadb*tar.gz`")],
+        ))
+    addPackageUploadStep(linux_connector_cpp, '"%(bindistname)s"')
+    return {'name': name, 'builddir': name,
+            'factory': linux_connector_cpp,
+            "slavenames": connector_slaves,
+            "category": "connectors"}
+
+bld_sles12_amd64_connector_cpp= bld_linux_connector_cpp_with_gcc5("ccpp-sles12-amd64", "vm-sles123-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON");
+
