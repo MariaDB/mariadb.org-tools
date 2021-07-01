b70d6c6ebb436cc41af6ca233778108e4c943509
Daniel Black <daniel@mariadb.org>
2021-07-01 18:24:41 +1000

    docker library build/test to stand alone script

---
+++ buildbot.mariadb.org/scripts/docker-library-build-and-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -xeuvo pipefail
+
+if [ -d mariadb-docker ]; then
+	pushd mariadb-docker
+	git pull
+	popd
+else
+	git clone https://github.com/MariaDB/mariadb-docker.git
+fi
+
+tarbuildnum=${1}
+mariadb_version=${2}
+mariadb_version=${mariadb_version#*-}
+buildername=${3:-amd64-ubuntu-2004-deb-autobake}
+master_branch=${4:-${mariadb_version%\.*}}
+image=mdb-test:$master_branch
+
+if [[ "$buildername" =~ 2004 ]]; then
+	base=focal
+else
+	base=bionic
+fi
+
+buildah bud --build-arg REPOSITORY="[trusted=yes] https://ci.mariadb.org/$tarbuildnum/$buildername/debs ./" --build-arg MARIADB_VERSION="1:$mariadb_version+maria~$base" --tag "$image" "mariadb-docker/$master_branch"
+
+mariadb-docker/.test/run.sh "$image"
+
