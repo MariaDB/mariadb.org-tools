ac60ec209b17f149f0e715cb0a1769c7075c131a
Daniel Black <daniel@mariadb.org>
2021-08-24 15:17:04 +1000

    bb.org: docker library, debug build + cleanup

--- buildbot.mariadb.org/scripts/docker-library-build-and-test.sh
+++ buildbot.mariadb.org/scripts/docker-library-build-and-test.sh
@@ -30,6 +30,25 @@ fi
 buildernamebase=${buildername#*-}
 builderarch=${buildername%%-*}
 
+declare -a annotations=(
+	"--annotation" "org.opencontainers.image.authors=MariaDB Foundation"
+	"--annotation" "org.opencontainers.image.documentation=https://hub.docker.com/_/mariadb"
+	"--annotation" "org.opencontainers.image.source=https://github.com/MariaDB/mariadb-docker/tree/$(cd mariadb-docker/$master_branch; git rev-parse HEAD)/$master_branch"
+	"--annotation" "org.opencontainers.image.licenses=GPL-2.0"
+	"--annotation" "org.opencontainers.image.title=MariaDB Server $master_branch CI build" \
+	"--annotation" "org.opencontainers.image.description=This is not a Release.\nBuild of the MariaDB Server from CI as of commit $commit" \
+	"--annotation" "org.opencontainers.image.version=$mariadb_version+$commit" \
+	"--annotation" "org.opencontainers.image.revision=$commit" )
+
+
+annotate()
+{
+	for item in "${annotations[@]}"
+	do
+		echo " --annotation" \""$item"\"
+	done
+}
+
 # Annotations - https://github.com/opencontainers/image-spec/blob/main/annotations.md#pre-defined-annotation-keys
 build()
 {
@@ -38,18 +57,14 @@ build()
 	t=$(mktemp)
 	buildah bud "$@" --build-arg REPOSITORY="[trusted=yes] https://ci.mariadb.org/$tarbuildnum/${arch}-${buildernamebase}/debs ./" \
 	       --build-arg MARIADB_VERSION="1:$mariadb_version+maria~$base" \
-	--annotation org.opencontainers.image.authors="MariaDB Foundation" \
-	--annotation org.opencontainers.image.documentation=https://hub.docker.com/_/mariadb \
-	--annotation org.opencontainers.image.source=https://github.com/MariaDB/mariadb-docker/tree/$(cd mariadb-docker/$master_branch; git rev-parse HEAD)/$master_branch \
-	--annotation org.opencontainers.image.licenses=GPL-2.0 \
-	--annotation org.opencontainers.image.title="MariaDB Server $master_branch CI build" \
-	--annotation org.opencontainers.image.description="This is not a Release.\nBuild of the MariaDB Server from CI as of commit $commit" \
-	--annotation org.opencontainers.image.version=$mariadb_version+$commit \
-	--annotation org.opencontainers.image.revision=$commit \
+		"${annotations[@]}" \
 	       "mariadb-docker/$master_branch" | tee "${t}"
 	image=$(tail -n 1 "$t")
 	rm "$t"
 }
+
+#
+# BUILD Image
 
 if [ "${builderarch}" = aarch64 ]
 then
@@ -57,6 +72,10 @@ then
 else
 	build "${builderarch}" --arch "${builderarch}"
 fi
+
+#
+# TEST Image
+#
 
 if [ "${builderarch}" != amd64 ]
 then
@@ -66,28 +85,83 @@ else
 fi
 mariadb-docker/.test/run.sh "$image"
 
-manifest=mariadb-devel-$master_branch-$commit
+#
+# METADATA:
 
-# create a manifest, and if it already exists, remove the one for the
-# current achitecuture as we're replacing this.
-# This could happen due to triggered rebuilds on buildbot.
+# Add manifest file of version and fix mariadb version in the configuration
+# because otherwise 'buildah manifest add "$devmanifest" "$image"' would be sufficient
 
-buildah manifest create "$manifest" || buildah manifest inspect "$manifest" \
-	| jq ".manifests[] | select( .platform.architecture == \"$builderarch\") | .digest" \
-	| xargs --no-run-if-empty -n 1 buildah manifest remove "$manifest"
+container=$(buildah from $image)
+manifestfile=$(mktemp)
+for item in "${annotations[@]}"
+  do
+    [ "$item" != "--annotation" ] && echo -e "$item\n"
+  done > "$manifestfile"
+buildah copy --add-history $container $manifestfile /manifest.txt
+rm -f "$manifestfile"
 
-buildah manifest add "$manifest" "$image"
+# which file - see mariadb-docker commit 710e0cd9d9197becc954e9a4c572cb97dd1d07a8
+if [[ $master_branch =~ 10.[234] ]]
+then
+	file=/etc/mysql/my.cnf
+else
+	file=/etc/mysql/mariadb.cnf
+fi
+# Set mariadb version according to semver
+buildah run --add-history $container  sed -ie '/^\[mariadb/a version='"$mariadb_version+$commit" $file
 
-buildah manifest annotate \
-	--annotation org.opencontainers.image.authors="MariaDB Foundation" \
-	--annotation org.opencontainers.image.documentation=https://hub.docker.com/_/mariadb \
-	--annotation org.opencontainers.image.source=https://github.com/MariaDB/mariadb-docker/tree/$(cd mariadb-docker/$master_branch; git rev-parse HEAD)/$master_branch \
-	--annotation org.opencontainers.image.licenses=GPL-2.0 \
-	--annotation org.opencontainers.image.title="MariaDB Server $master_branch CI build" \
-	--annotation org.opencontainers.image.description="This is not a Release.\nBuild of the MariaDB Server from CI as of commit $commit" \
-	--annotation org.opencontainers.image.version=$mariadb_version+$commit \
-	--annotation org.opencontainers.image.revision=$commit \
-	"$manifest" "$image"
+#
+# MAKE it part of the mariadb-devel manifest
+#
+
+buildmanifest()
+{
+	manifest=$1
+	shift
+	container=$1
+	shift
+	# create a manifest, and if it already exists, remove the one for the
+	# current architecuture as we're replacing this.
+	# This could happen due to triggered rebuilds on buildbot.
+
+	buildah manifest create "$manifest" || buildah manifest inspect "$manifest" \
+		| jq ".manifests[] | select( .platform.architecture == \"$builderarch\") | .digest" \
+		| xargs --no-run-if-empty -n 1 buildah manifest remove "$manifest"
+
+	t=$(mktemp)
+	buildah commit "$@" --iidfile "$t" --manifest "$manifest" "$container"
+	image=$(<$t)
+	# $image is the wrong sha for annotation. Config vs Blogb?
+	# Even below doesn't annotate manifest. Unknown reason, doesn't error
+	buildah manifest inspect "$manifest" \
+		| jq ".manifests[] | select( .platform.architecture == \"$builderarch\") | .digest" \
+		| xargs --no-run-if-empty -n 1 buildah manifest annotate \
+			"${annotations[@]}" \
+			"$manifest"
+	rm -f "$t"
+}
+
+devmanifest=mariadb-devel-$master_branch-$commit
+
+buildmanifest $devmanifest $container
+
+#
+# MAKE Debug manifest
+
+# linux-tools-common for perf
+# TODO: ci repo generation fix needed
+# buildah run --add-history "$container" sh -c \
+# 	"apt update \
+# 	&& apt-get install -y linux-tools-common gdbserver \
+# 	&& dpkg-query  --showformat='\${Package},\${Version},\${Architecture}\n' --show | grep mariadb \
+# 	| while IFS=, read  pkg version arch; do \
+#           [ \$arch != all ] apt-get install -y \${pkg}-dbgsym=\${version} ;
+#         done; \
+# 	rm -rf /var/lib/apt/lists/*"
+
+debugmanifest=mariadb-debug-$master_branch-$commit
+
+buildmanifest $debugmanifest $container --rm
 
 if [[ $master_branch =~ 10.[234] ]]
 then
@@ -96,10 +170,14 @@ else
 	expected=4
 fi
 
-if [[ $(buildah manifest inspect "$manifest" | jq '.manifests | length') -ge $expected ]]
-then
-	podman manifest push "$manifest" "docker://quay.io/mariadb-foundation/mariadb-devel:$master_branch"
+#
+#
+# PUSHIT - if the manifest if complete, i.e. all supported arches are there, we push
+#
 
+manifestcleanup()
+{
+	manifest=$1
 	t=$(mktemp)
 	buildah manifest inspect "$manifest" | tee "${t}"
 	# A manifest is an image type that podman can remove
@@ -116,6 +194,22 @@ then
 		done
 	rm "$t"
 	podman rmi "$manifest"
+}
+
+if [[ $(buildah manifest inspect "$devmanifest" | jq '.manifests | length') -ge $expected ]]
+then
+	podman manifest push "$devmanifest" "docker://quay.io/mariadb-foundation/mariadb-devel:$master_branch"
+	#podman manifest push "$debugmanifest" "docker://quay.io/mariadb-foundation/mariadb-debug:$master_branch"
+
+	manifestcleanup "$devmanifest"
+	#manifestcleanup "$debugmanifest"
+
+	podman images
+	# lost and forgotten (or just didn't make enough manifest items - build failure on an arch)
+	lastweek=$(date +%s --date='1 week ago')
+	podman images --format=json | jq ".[] | select(.Created <= $lastweek and .Dangling) | .Id" | xargs --no-run-if-empty podman rmi
+	# size is a poor criteria - excludes ubuntu base images however.
+	podman images --format=json | jq ".[] | select(.Created <= $lastweek and .Size < 10000 ) | .Id" | xargs --no-run-if-empty buildah manifest rm
 	podman images
 fi
 
