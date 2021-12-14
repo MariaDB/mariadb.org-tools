8461e376b875c79a12e4f0f13e86432316145420
Elena Stepanova <elenst@mariadb.com>
2021-12-14 23:43:32 +0200

    Use the last buildbot builds of Galera library for tests

--- buildbot/steps/minor_upgrade.deb.sh
+++ buildbot/steps/minor_upgrade.deb.sh
@@ -373,7 +373,7 @@ esac
 
 mkdir galera_download
 cd galera_download
-if ! wget https://hasky.askmonty.org/builds/mariadb-${GALERA_VERSION}.x/latest/kvm-deb-${version_name}-${arch}-gal/debs/ --recursive -np -R "index.html*" -nH --cut-dirs=4 ; then
+if ! wget https://hasky.askmonty.org/builds/mariadb-${GALERA_VERSION}.x/latest/kvm-deb-${version_name}-${arch}-gal/debs/ --recursive -np -R "index.html*" -nH --cut-dirs=4 --no-check-certificate ; then
   echo "ERROR: Could not download the Galera library"
   exit 1
 fi
