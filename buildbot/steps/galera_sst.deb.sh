0c200639c32fce09c6b5eee89afe73ed44ed6e58
Elena Stepanova <elenst@mariadb.com>
2022-02-22 03:58:03 +0200

    Increase waiting time

--- buildbot/steps/galera_sst.deb.sh
+++ buildbot/steps/galera_sst.deb.sh
@@ -202,7 +202,7 @@ for node in 2 3 ; do
   set +x
   echo "Waiting till node $node comes up..."
   for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 ; do
-    sleep 3
+    sleep 5
     if mysql -uroot -prootpass --port=830$node --protocol=tcp -e "select * from mgc.t1" ; then
       res=0
       break
