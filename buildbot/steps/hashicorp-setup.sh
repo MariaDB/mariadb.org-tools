0846ba1f146eefa0cbc834331ef71ea095174d7c
Elena Stepanova <elenst@mariadb.com>
2022-04-04 23:51:24 +0300

    MDEV-28025 - Vault Key Management Plugin testing

---
+++ buildbot/steps/hashicorp-setup.sh
@@ -0,0 +1,60 @@
+########################################################################
+# Hashicorp vault configuration on Linux
+########################################################################
+
+. /etc/os-release
+linux=${ID}
+if [ "${linux}" == "rocky" ] || [ "${linux}" == "centos" ]  ; then
+  linux=rhel
+fi
+
+if [ "${linux}" == "debian" ] || [ "${linux}" == "ubuntu" ] ; then
+  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+  sudo apt-get update && sudo apt-get install vault
+elif [ "${linux}" == "rhel" ] ; then
+  sudo yum install -y yum-utils
+  sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+  sudo yum -y install vault
+else
+  echo "WARNING: Don't know how to install vault on ${linux}, skipping hashicorp tests"
+  exit 0
+fi
+
+if which vault ; then
+  cat << EOF > /tmp/vault.hcl
+storage "file" {
+  path = "/opt/vault/data"
+}
+
+listener "tcp" {
+  address       = "0.0.0.0:8200"
+  tls_disable = 1
+}
+
+api_addr = "http://127.0.0.1:8200"
+
+max_lease_ttl = "730h"
+default_lease_ttl = "730h"
+max_versions=2
+ui = true
+log_level = "Trace"
+EOF
+
+  sudo cp /tmp/vault.hcl /etc/vault.d/
+  export VAULT_ADDR='http://127.0.0.1:8200'
+  sudo systemctl restart vault
+  # restart exits too early, vault may be not ready yet
+  for i in 1 2 3 4 5 ; do
+    sleep 2
+    if vault operator init > /tmp/vault.init ; then
+      break
+    fi
+  done
+
+  vault operator unseal `grep 'Unseal Key 1:' /tmp/vault.init | awk '{ print $4 }'`
+  vault operator unseal `grep 'Unseal Key 2:' /tmp/vault.init | awk '{ print $4 }'`
+  vault operator unseal `grep 'Unseal Key 3:' /tmp/vault.init | awk '{ print $4 }'`
+  export VAULT_TOKEN=`grep 'Initial Root Token:' /tmp/vault.init | awk '{ print $4 }'`
+  vault login $VAULT_TOKEN
+fi
