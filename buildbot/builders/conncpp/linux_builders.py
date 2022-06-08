0292eee3047ab58ca53367507e59bf9c075a68c0
Daniel Bartholomew <dbart@mariadb.com>
2022-06-08 20:10:57 +0300

    MDEV-28726 - Add RHEL-9 builders

--- buildbot/builders/conncpp/linux_builders.py
+++ buildbot/builders/conncpp/linux_builders.py
@@ -68,6 +68,7 @@ conncpp_linux_step4_testsrun
 ######################## bld_linux_connector_cpp - END #####################
 
 ######################## "Normal" builders ######################
+bld_rhel9_amd64_connector_cpp= bld_linux_connector_cpp("ccpp-rhel9-amd64", "vm-rhel9-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON ");
 bld_centos8_amd64_connector_cpp= bld_linux_connector_cpp("ccpp-centos8-amd64", "vm-rhel8-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON ");
 bld_stretch_amd64_connector_cpp= bld_linux_connector_cpp("ccpp-stretch-amd64", "vm-stretch-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON ");
 
