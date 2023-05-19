9a63a0c23534f7ac7dcd18371b92d7e37c056163
Anel Husakovic <anel@mariadb.org>
2023-05-19 12:06:49 +0200

    jdbc_blog: try to add Oracle JDBC - not working
    - Note entrypoint doesn't work, you need to add manually tables
    - Actually It say it works (3 rows inserted), but not visible result
    - JDBC connection with MariaDB is not working

---
+++ anel/jdbc_connect_blog/oracle_data/data.sql
@@ -0,0 +1,2 @@
+CREATE TABLE oracle_t(t number);
+INSERT all into oracle_t(t) values (10) into oracle_t(t) values (20) into oracle_t(t) values(30) select 1 from dual;
--- anel/jdbc_connect_blog/target_data/data.sql
+++ anel/jdbc_connect_blog/target_data/data.sql
@@ -13,3 +13,21 @@ insert into t values (1),(2);
 # check hostname -I first and change connection url
 create table db_mariadb_target engine=connect table_type=JDBC tabname=t_maria connection='jdbc:mariadb://mariadb-source/db_maria?user=root&password';
 create table db_mysql_target engine=connect table_type=JDBC tabname=t_mysql connection='jdbc:mariadb://mysql-source/db_mysql?user=root&password';
+
+create server 'oracle1' foreign data wrapper 'oracle' options (
+HOST 'jdbc:oracle:thin:@oracle-source:1521:xe',
+DATABASE 'XE',
+USER 'system',
+PASSWORD 'oracle',
+PORT 0,
+SOCKET '',
+OWNER 'SYSTEM');
+
+create server 'oracle2' foreign data wrapper 'oracle' options (
+HOST 'jdbc:oracle:thin:@oracle-source:1521:xe',
+DATABASE 'XEPDB1',
+USER 'system',
+PASSWORD 'oracle',
+PORT 0,
+SOCKET '',
+OWNER 'SYSTEM');
