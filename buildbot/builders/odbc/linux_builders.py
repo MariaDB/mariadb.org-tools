bf967a674806c3e9596096683bd9e84f55af02d2
Daniel Bartholomew <dbart@mariadb.com>
2023-06-29 03:03:22 +0000

    automatic builders/odbc/linux_builders.py commit

--- buildbot/builders/odbc/linux_builders.py
+++ buildbot/builders/odbc/linux_builders.py
@@ -343,11 +343,12 @@ for i in 1 2 3 4 5 6 7 8 9 10 ; do
 done
 
 sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get install --allow-unauthenticated -y ./%(bindistname)s"
+sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get install --allow-unauthenticated -y unixodbc"
 export CFLAGS="${CFLAGS}"""+ cflags + """" """ +
 connodbc_linux_step0_checkout + """
 rm -rf ../src/libmariadb
 sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get install --allow-unauthenticated -y cmake"
-cmake -DDEB=On -DCPACK_GENERATOR=DEB -DBUILD_TESTS_ONLY=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DMARIADB_LINK_DYNAMIC=On -DCMAKE_C_FLAGS_RELWITHDEBINFO="-L/usr/local/lib/mariadb" """ + cmake_params + """ ../src
+cmake -DBUILD_TESTS_ONLY=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DMARIADB_LINK_DYNAMIC=On -DCMAKE_C_FLAGS_RELWITHDEBINFO="-L/usr/local/lib/mariadb" """ + cmake_params + """ ../src
 cmake --build . --config RelWithDebInfo
 """ + connodbc_linux_step2_serverinstall + connodbc_linux_step4_testsrun)]))
     return {'name': name, 'builddir': name,
@@ -360,138 +361,4 @@ bld_rhel8_x64_connector_odbc_rpm= build_connector_odbc_rpm("codbc-rhel8-amd64-rp
 bld_rhel9_x64_connector_odbc_rpm= build_connector_odbc_rpm("codbc-rhel9-amd64-rpm", "vm-rhel9-amd64", "", " -DWITH_SSL=OPENSSL");
 
 bld_codbc_focal_amd64_deb= build_connector_odbc_deb("codbc-focal-amd64-deb", "vm-focal-amd64", "", " -DWITH_SSL=OPENSSL");
-
-################################# bld_linux_connector_oddbc ################################
-def bld_linux_connector_odbc(name, kvm_image, cflags, yum, conc_branch, cmake_params, tag):
-    linux_connector_odbc= BuildFactory()
-    args= ["--port="+getport(), "--user=buildbot", "--smp=4", "--cpu=host"]
-    linux_connector_odbc.addStep(ShellCommand(
-        description=["cleaning", "build", "dir"],
-        descriptionDone=["clean", "build", "dir"],
-        command=["sh", "-c", "rm -Rf ../build/*"]))
-    linux_connector_odbc.addStep(ShellCommand(
-        description=["rsyncing", "VMs"],
-        descriptionDone=["rsync", "VMs"],
-        doStepIf=(lambda(step): step.getProperty("slavename") != "bb01"),
-        haltOnFailure=True,
-        command=["rsync", "-a", "-v", "-L",
-                 "bb01.mariadb.net::kvm/vms/"+kvm_image+"-build.qcow2",
-                 "/kvm/vms/"]))
-    linux_connector_odbc.addStep(Compile(
-        description=["building", "linux-connctor_odbc"],
-        descriptionDone=["build", "linux-connector_odbc"],
-        timeout=3600,
-        env={"TERM": "vt102"},
-        command=["runvm", "--base-image=/kvm/vms/"+kvm_image+"-build.qcow2"] + args +["vm-tmp-"+getport()+".qcow2",
-        "rm -Rf buildbot && mkdir buildbot",
-        WithProperties("""
-set -ex
-git --version
-rm -Rf build
-""" + (""" sudo yum --disablerepo=epel -y install unixODBC
-sudo yum -y install unixODBC-devel
-""" if yum else """#sudo apt-get update
-sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get install --allow-unauthenticated -y --force-yes -m unixodbc-dev"
-sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get install --allow-unauthenticated -y --force-yes -m openssl libssl-dev"
-""") + """export CFLAGS="${CFLAGS}"""+ cflags + """"
-time git clone --depth 1 -b %(branch)s "https://github.com/MariaDB/mariadb-connector-odbc.git" build
-[-z "%(revision)s"] && git checkout %(revision)s
-cd build
-rm -rf ./test
-git submodule init
-git submodule update
-cd libmariadb
-git fetch --all --tags --prune
-git log | head -n5
-cd ..
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCONC_WITH_UNIT_TESTS=Off -DPACKAGE_PLATFORM_SUFFIX=$HOSTNAME -DWITH_UNIT_TESTS=Off""" + cmake_params + """ .
-cmake --build . --config RelWithDebInfo --target package
-"""),
-        "= scp -r -P "+getport()+" "+kvm_scpopt+" buildbot@localhost:/home/buildbot/build/mariadb*tar.gz .",
-        ]))
-    linux_connector_odbc.addStep(SetPropertyFromCommand(
-        property="bindistname",
-        command=["sh", "-c", WithProperties("basename `ls mariadb*odbc*tar.gz`")],
-        ))
-    addPackageUploadStep(linux_connector_odbc, '"%(bindistname)s"')
-    return {'name': name, 'builddir': name,
-            'factory': linux_connector_odbc,
-            "slavenames": connector_slaves,
-            "category": "connectors"}
-######################## bld_linux_connector_oddbc - END #####################
-#sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get install --allow-unauthenticated -y git"
-#sudo yum --disablerepo=epel -y install git
-
-######################## Old 2.0 builder ######################
-#bld_linux_x64_connector_odbc= bld_linux_connector_odbc("linux_x64-connector-odbc", "vm-centos6-amd64", "", True, "connector_c_2.3", " -DWITH_OPENSSL=OFF -DSYSTEM_NAME=rhel6 ", "v_2.3.7");
-
-
-
-def bld_xcomp_linux_connector_odbc(name, kvm_image, conc_branch, cmake_params, tag):
-    linux_connector_odbc= BuildFactory()
-    args= ["--port="+getport(), "--user=buildbot", "--smp=4", "--cpu=host"]
-    linux_connector_odbc.addStep(ShellCommand(
-        description=["cleaning", "build", "dir"],
-        descriptionDone=["clean", "build", "dir"],
-        command=["sh", "-c", "rm -Rf ../build/*"]))
-    linux_connector_odbc.addStep(ShellCommand(
-        description=["rsyncing", "VMs"],
-        descriptionDone=["rsync", "VMs"],
-        doStepIf=(lambda(step): step.getProperty("slavename") != "bb01"),
-        haltOnFailure=True,
-        command=["rsync", "-a", "-v", "-L",
-                 "bb01.mariadb.net::kvm/vms/"+kvm_image+"-build.qcow2",
-                 "/kvm/vms/"]))
-    linux_connector_odbc.addStep(Compile(
-        description=["building", "centos7-connctor_odbc"],
-        descriptionDone=["build", "centos7-connector_odbc"],
-        timeout=3600,
-        env={"TERM": "vt102"},
-        command=["runvm", "--base-image=/kvm/vms/"+kvm_image+"-build.qcow2"] + args +["vm-tmp-"+getport()+".qcow2",
-        "rm -Rf buildbot && mkdir buildbot",
-        WithProperties("""
-set -ex
-rm -Rf build
-#sudo yum -y install git
-sudo yum --disablerepo=epel -y install git
-#sudo yum -y install unixODBC.x86_64
-sudo yum -y install unixODBC.i686
-#sudo yum -y install unixODBC-devel.x86_64
-sudo yum -y install unixODBC-devel.i686
-#sudo yum -y install zlib.x86_64
-sudo yum -y install glibc-devel.i686 libstdc++-devel.i686 zlib.i686
-#sudo yum -y install glibc-devel.x86_64 libstdc++-devel.x86_64
-sudo yum -y install openssl-devel.i686
-#sudo yum -y install openssl-devel.x86_64
-sudo yum -y install libcom_err.i686
-#sudo yum -y install libcom_err.x86_64
-sudo ldconfig
-time git clone --depth 1 -b %(branch)s "https://github.com/MariaDB/mariadb-connector-odbc.git" build
-cd build
-rm -rf ./test
-git submodule init
-git submodule update
-cd libmariadb
-git fetch --all --tags --prune
-git log | head -n5
-cd ..
-setarch i386 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DPACKAGE_PLATFORM_SUFFIX=$HOSTNAME -DWITH_UNIT_TESTS=Off """ + cmake_params + """ -DCMAKE_TOOLCHAIN_FILE=cmake/linux_x86_toolchain.cmake .
-setarch i386 cmake --build . --config RelWithDebInfo --target package
-setarch i386 make package
-"""),
-        "= scp -r -P "+getport()+" "+kvm_scpopt+" buildbot@localhost:/home/buildbot/build/mariadb*tar.gz .",
-        ]))
-    linux_connector_odbc.addStep(SetPropertyFromCommand(
-        property="bindistname",
-        command=["sh", "-c", WithProperties("basename `ls mariadb*odbc*tar.gz`")],
-        ))
-    addPackageUploadStep(linux_connector_odbc, '"%(bindistname)s"')
-    return {'name': name, 'builddir': name,
-            'factory': linux_connector_odbc,
-            "slavenames": connector_slaves,
-            "category": "connectors"}
-
-#bld_centos7_x86_connector_odbc= bld_xcomp_linux_connector_odbc("centos7_x86-connector-odbc", "vm-centos7-amd64", "connector_c_2.3", " -DWITH_OPENSSL=OFF -DSYSTEM_NAME=rhel7 ", "v_2.3.7");
-bld_centos7_x86_connector_odbc_new= bld_xcomp_linux_connector_odbc("codbc-centos7-x86", "vm-centos74-amd64", "3.0", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON -DSYSTEM_NAME=rhel7 ", "v3.1.7");
-bld_rhel8_x86_connector_odbc= bld_xcomp_linux_connector_odbc("codbc-rhel8-x86", "vm-rhel8-amd64", "3.0", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON -DSYSTEM_NAME=rhel8 ", "v3.1.7");
 
