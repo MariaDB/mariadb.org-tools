547b7345c03238fa49778867872ec0da69817f75
Daniel Bartholomew <dbart@mariadb.com>
2024-04-30 03:03:23 +0000

    automatic builders/odbc/linux_builders.py commit

--- buildbot/builders/odbc/linux_builders.py
+++ buildbot/builders/odbc/linux_builders.py
@@ -236,6 +236,9 @@ bld_codbc_lunar_amd64= build_linux_connector_odbc_no_test("codbc-lunar-amd64", "
 
 bld_codbc_mantic_amd64= build_linux_connector_odbc_no_test("codbc-mantic-amd64", "vm-mantic-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON");
 
+bld_codbc_noble_amd64= build_linux_connector_odbc_no_test("codbc-noble-amd64", "vm-noble-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON");
+bld_codbc_noble_aarch64= build_linux_connector_odbc_no_test("codbc-noble-aarch64", "vm-noble-aarch64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON", slaves=connector_slaves_aarch64);
+
 bld_codbc_fedora37_amd64= build_linux_connector_odbc_no_test("codbc-fedora37-amd64", "vm-fedora37-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON");
 bld_codbc_fedora38_amd64= build_linux_connector_odbc_no_test("codbc-fedora38-amd64", "vm-fedora38-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON");
 bld_codbc_fedora39_amd64= build_linux_connector_odbc_no_test("codbc-fedora39-amd64", "vm-fedora39-amd64", "", " -DWITH_SSL=OPENSSL -DWITH_OPENSSL=ON");
